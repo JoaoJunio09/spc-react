7c8c6c7ad8cc5641bf0db3382479d5cf9c6983b3
JoaoJunio09 <joaojunio818@gmail.com>
2026-05-16 20:00:14 -0300

    Filtrando catequizandos

--- src/features/catechumens/components/Catechumens.tsx
+++ src/features/catechumens/components/Catechumens.tsx
@@ -1,11 +1,23 @@
+import { toast } from 'react-toastify';
 import type { StepResponse } from '../../../interfaces/step/StepResponse';
 import { FormatStep } from '../../../utils/FormatStep';
 import useLoadSelectFilter from '../hooks/useLoadSelectFilter';
 import '../styles/catechumens.css';
+import useFilter from '../hooks/useFilter';
 
 function Catechumens() {
 
-	const { steps, error } = useLoadSelectFilter();
+	const { steps, error: errorFilter } = useLoadSelectFilter();
+	const {
+		filter,
+		setStepId,
+		setCatechistId,
+		catechumens,
+		loading,
+		error
+	} = useFilter();
+
+	console.log(catechumens);
 
 	function defineOptionOfCatechists(step: StepResponse) {
 		let textOption:string = '';
@@ -23,8 +35,19 @@ function Catechumens() {
 		return [textOption];
 	}
 
+	function handleFilter(e: React.ChangeEvent<HTMLSelectElement>) {
+		const value = e.target.value.split('-');
+		
+		const stepId = value[0];
+		const catechistId = value[1];
+
+		setStepId(Number(stepId));
+		setCatechistId(Number(catechistId));
+	}
+
 	return (
 		<main className="container-catechumens catequizandos-page">
+			{errorFilter && toast.error(errorFilter)}
 			<section className="page-intro">
 				<h1>Catequizandos</h1>
 				<p>Consulte a frequência dos catequizandos filtrando por etapa e catequista.</p>
@@ -35,14 +58,14 @@ function Catechumens() {
 					<div className="filtros-main">
 						<div className="filtro-group">
 							<label htmlFor="filtro-etapa">Etapa e Catequista</label>
-							<select id="filter-step-and-catechist">
+							<select id="filter-step-and-catechist" onChange={(e) => handleFilter(e)}>
 								<option hidden>[Selecione a Etapa e o Catequista]</option>
 								{steps.map(step => (
 									step.catechists.length === 1
-										? <option value={`${step.id}-${step.catechists[0].id}`}>
+										? <option key={`${step.id}-${step.catechists[0].id}`} value={`${step.id}-${step.catechists[0].id}`}>
 												{FormatStep.format(step.stepName)} - {step.catechists[0].firstName} {step.catechists[0].lastName}
 											</option>
-										: <option value={`${step.id}-${step.catechists[0].id}`}>
+										: <option key={`${step.id}-${step.catechists[0].id}`} value={`${step.id}-${step.catechists[0].id}`}>
 												{defineOptionOfCatechists(step)}
 											</option>
 								))}
@@ -54,7 +77,7 @@ function Catechumens() {
 
 			<section className="tabela-section">
 				<div className="tabela-container">
-					<table className="tabela-catequizandos" id="tabela-dados" style={{ display: 'none' }}>
+					<table className="tabela-catequizandos" id="tabela-dados" style={{ display: `${catechumens.length === 0 ? 'none' : 'initial'}` }}>
 						<thead>
 							<tr>
 								<th>Nome do Catequizando</th>
@@ -65,11 +88,27 @@ function Catechumens() {
 							</tr>
 						</thead>
 						<tbody>
-								
+							{catechumens.map(catechumen => (
+								<tr key={catechumen.id} className="catequizando-row">
+									<td id="firstName">{catechumen.firstName} {catechumen.lastName}</td>
+									<td id="step">{FormatStep.format(catechumen.step.stepName)}</td>
+									<td id="catechistFirstName">{catechumen.step.catechists[0].firstName}</td>
+									<td className="text-center">
+										<div className="freq-wrapper">
+											<span className="freq-badge freq-high">{catechumen.currentFrequency}%</span>
+										</div>
+									</td>
+									<td className="text-center">
+										<div className="freq-wrapper">
+											<span className="freq-badge freq-medium">{catechumen.totalFrequency}%</span>
+										</div>
+									</td>
+								</tr>
+							))}
 						</tbody>
 					</table>
 
-					<div className="estado-vazio" id="estado-vazio-filtros">
+					<div className="estado-vazio" id="estado-vazio-filtros" style={{ display: `${catechumens.length === 0 ? 'initial' : 'none'}` }}>
 						<div className="vazio-content">
 							<i data-lucide="users" className="vazio-icon"></i>
 							<h3>Filtre os catequizandos por ETAPA e CATEQUISTA</h3>
---
+++ src/features/catechumens/hooks/useFilter.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import type { CatechumenResponse } from "../../../interfaces/catechumen/CatechumenResponse";
+import InternalServerError from "../../../exceptions/server/InternalServerError";
+import CatechumenService from "../../../services/CatechumenService";
+
+function useFilter() {
+	const [catechumens, setCatechumens] = useState<CatechumenResponse[]>([]);
+	const [stepId, setStepId] 					= useState<number>();
+	const [catechistId, setCatechistId] = useState<number>();
+	const [loading, setLoading] 				= useState<boolean>(false);
+	const [error, setError] 						= useState<string | null>(null);
+
+	const catechumenService: CatechumenService = new CatechumenService();
+
+	useEffect(() => {
+		if (!stepId || !catechistId) return;
+		
+		filter();
+	}, [stepId, catechistId]);
+
+	async function filter() {
+		try {
+			console.log(stepId);
+			console.log(catechistId);
+			setLoading(true);
+			setError(null);
+
+			const data = await catechumenService.getAll({ stepId: stepId, catechistId: catechistId });
+			setCatechumens(data);
+		}
+		catch (err) {
+			if (err instanceof InternalServerError) {
+				setError(err.message)
+			}
+			else {
+				setError('Erro inesperado - Servidor Indísponivel')
+			}
+		}
+		finally {
+			setLoading(false);
+		}
+	}
+
+	return {
+		filter,
+		setStepId,
+		setCatechistId,
+		catechumens,
+		loading,
+		error
+	}
+}
+
+export default useFilter;
