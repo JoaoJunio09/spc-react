0864be464bc0063f509eb867686b1c02f4735775
JoaoJunio09 <joaojunio818@gmail.com>
2026-06-12 12:04:41 -0300

    notificações em realtime

--- src/hooks/useWebSocket.ts
+++ src/hooks/useWebSocket.ts
@@ -14,44 +14,52 @@ function useWebSocket(onMessage: MessageHandler) {
 
 		const client = new Client({
 			webSocketFactory: () =>
-				new SockJs(`${import.meta.env.VITE_API_URL_PROD}/ws`),
-				
-				connectHeaders: {
-					Authorization: `Bearer ${auth.accessToken}`,
-				},
+				new SockJs(`${import.meta.env.VITE_API_URL_DEV}/ws`),
 
-				reconnectDelay: 5000,
+			connectHeaders: {
+				Authorization: `Bearer ${auth.accessToken}`,
+			},
 
-				onConnect: () => {
-					client.subscribe('/user/queue/notifications', (message) => {
+			reconnectDelay: 5000,
+
+			onConnect: () => {
+				client.subscribe('/user/queue/notifications', (message) => {
+					onMessage(message.body);
+				});
+
+				if (hasRole('ROLE_COORDINATOR')) {
+					client.subscribe('/topic/coordinator', (message) => {
 						onMessage(message.body);
 					});
-
-					if (hasRole('ROLE_COORDINATOR')) {
-						client.subscribe('/topic/coordinators', (message) => {
-							onMessage(message.body);
-						});
-					}
-
-					if (hasRole('ROLE_ADMIN')) {
-						client.subscribe('/topic/admin', (message) => {
-							onMessage(message.body);
-						});
-					}
-				},
-
-				onDisconnect: () => {
-					console.log('WebSocket desconectado');
-				},
-
-				onStompError: (frame) => {
-					console.log('Erro STOMP: ', frame.headers['message']);
 				}
+
+				if (hasRole('ROLE_ADMIN')) {
+					client.subscribe('/topic/admin', (message) => {
+						onMessage(message.body);
+					});
+				}
+			},
+
+			// onDisconnect: () => {
+			// 	console.log('WebSocket desconectado');
+			// },
+
+			// onWebSocketClose: (event) => {
+			// 	console.log("🔌 Socket fechado", event);
+			// },
+
+			// onWebSocketError: (event) => {
+			// 	console.log("🚨 Erro no socket", event);
+			// },
+
+			onStompError: (frame) => {
+				console.log('Erro STOMP: ', frame.headers['message']);
+			}
 		});
 
 		client.activate();
 		clientRef.current = client;
-	}, [auth?.accessToken]);
+	}, [auth?.accessToken, hasRole]);
 
 	useEffect(() => {
 		connect();
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { Token } from "../data/auth/Token";
 
 const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL_PROD
+	baseURL: import.meta.env.VITE_API_URL_DEV
 });
 
 api.interceptors.request.use((config) => {
